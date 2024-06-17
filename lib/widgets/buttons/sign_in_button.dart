e5be04fc64487fd4c66ccc150fb578a217992b47
abuyukyi101198 <acb3@outlook.com>
2024-06-17 11:15:18 +0300

    sign in button added

---
+++ lib/widgets/buttons/sign_in_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: () {},
+      child: const Text('Sign in')
+    );
+  }
+}
