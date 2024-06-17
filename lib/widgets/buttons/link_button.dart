04512b1d97951dd85858e18869ca9b1326170ac8
abuyukyi101198 <acb3@outlook.com>
2024-06-17 14:49:06 +0300

    added underline on press

--- lib/widgets/buttons/link_button.dart
+++ lib/widgets/buttons/link_button.dart
@@ -2,23 +2,35 @@ import 'package:flutter/material.dart';
 import '../../utils/color_palette.dart';
 
 class LinkButton extends StatelessWidget {
+  final VoidCallback? onPressed;
   final String label;
 
-  const LinkButton({super.key, required this.label});
+  const LinkButton({super.key, required this.onPressed, required this.label});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ButtonStyle(
-          overlayColor:
-          WidgetStateProperty.all(Colors.transparent)),
-      child: Text(
-        label,
-        style: const TextStyle(
-            color: ColorPalette.primaryColor,
-            fontWeight: FontWeight.w700),
+        overlayColor: WidgetStateProperty.resolveWith((states) => Colors.transparent),
+        padding: WidgetStateProperty.all(EdgeInsets.zero),
+        alignment: Alignment.centerLeft,
+        textStyle: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.pressed)) {
+            return const TextStyle(
+              decoration: TextDecoration.underline,
+              decorationThickness: 2
+            );
+          }
+          return null;
+        }),
       ),
+      child: Text(label, style: const TextStyle(
+        color: ColorPalette.primaryColor,
+        fontFamily: 'Sharp Grotesk',
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+      )),
     );
   }
 }
