638751f8610fd3deafbf71a6a5bf358f3c44859c
abuyukyi101198 <acb3@outlook.com>
2024-06-20 16:51:55 +0300

    created upcoming_match_card.dart

--- lib/screens/matches_screen.dart
+++ lib/screens/matches_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sidelines/utils/color_palette.dart';
-import 'package:sidelines/widgets/display/upcoming_team_display.dart';
-import 'package:sidelines/widgets/display/upcoming_time_display.dart';
+import 'package:sidelines/widgets/display/upcoming_match_card.dart';
 import 'package:sidelines/widgets/misc/navigation_item.dart';
 
 class MatchesScreen extends StatelessWidget {
@@ -16,43 +14,14 @@ class MatchesScreen extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.w700),
         ),
       ),
-      body: Center(
+      body: const Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: const BoxDecoration(
-                    color: ColorPalette.primaryColor,
-                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                  ),
-                  width: double.infinity,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          UpcomingTeamDisplay(
-                            teamName: 'Tarkington',
-                            teamBadgeUrl:
-                                'https://cdn.dribbble.com/users/1341408/screenshots/16441374/media/bfa8046d1062a3abe131eefe64653f59.jpg?resize=1600x1200&vertical=center',
-                          ),
-                          UpcomingTeamDisplay(
-                            teamName: 'Hillenbrand',
-                            teamBadgeUrl:
-                            'https://cdn.dribbble.com/userupload/4247758/file/original-b54882b8a6d01145e2792fa80104ba3e.jpg?resize=2048x1536',
-                          ),
-                        ],
-                      ),
-                      UpcomingTimeDisplay(
-                          matchDateTime:
-                              DateTime.now().add(const Duration(days: 2)))
-                    ],
-                  ))
+              UpcomingMatchCard(),
             ],
           ),
         ),
