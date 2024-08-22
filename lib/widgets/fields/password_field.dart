61e0d5c980b93b5ef4e2f7c4b00e2abe97135230
abuyukyi101198 <acb3@outlook.com>
2024-08-22 20:38:41 +0300

    added controller and validator for password_field.dart

--- lib/widgets/fields/password_field.dart
+++ lib/widgets/fields/password_field.dart
@@ -3,17 +3,30 @@ import 'package:sidelines/widgets/fields/text_field_with_label.dart';
 
 class PasswordField extends StatelessWidget {
   final String? label;
+  final TextEditingController? controller;
 
   final String _defaultLabel = 'Password';
 
-  const PasswordField({super.key, this.label});
+  const PasswordField({super.key, this.label, this.controller});
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+    if (value.length < 8) {
+      return 'Password must be at least 8 characters long';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     final String effectiveLabel = label ?? _defaultLabel;
     return TextFieldWithLabel(
       label: effectiveLabel,
+      controller: controller,
       textInputType: TextInputType.visiblePassword,
+      validator: _validatePassword,
     );
   }
 }
