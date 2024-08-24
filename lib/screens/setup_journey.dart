9934052086e60d31b564ebd628e02444ee931ddb
abuyukyi101198 <acb3@outlook.com>
2024-08-25 00:39:45 +0300

    fixed page change error by adding listener

--- lib/screens/setup_journey.dart
+++ lib/screens/setup_journey.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sidelines/data/storage.dart';
 import 'package:sidelines/screens/welcome_screen.dart';
 import 'package:sidelines/utils/color_palette.dart';
-import 'package:sidelines/widgets/buttons/next_bottom_button.dart';
+import 'package:sidelines/widgets/footers/setup_journey_footer.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:sidelines/screens/username_screen.dart';
 import 'package:sidelines/screens/personal_info_screen.dart';
@@ -20,6 +20,26 @@ class SetupJourney extends StatefulWidget {
 class SetupJourneyState extends State<SetupJourney> {
   final PageController _pageController = PageController();
   final TextEditingController _usernameController = TextEditingController();
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      if (_pageController.page?.round() != currentIndex) {
+        setState(() {
+          currentIndex = _pageController.page!.round();
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _usernameController.dispose();
+    super.dispose();
+  }
 
   Future<bool> _checkUsernameValidity(String username) async {
     if (username.isEmpty) {
@@ -28,11 +48,42 @@ class SetupJourneyState extends State<SetupJourney> {
     final token = await Storage().read('token');
     final response = await http.post(
       Uri.parse('${Constants.baseApiUrl}username-unique-check/'),
-      headers: {'Content-Type': 'application/json', 'Authorization': 'Token $token'},
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token $token'
+      },
       body: jsonEncode({'username': username}),
     );
 
     return response.statusCode == 200;
+  }
+
+  void _goBack() {
+    if (_pageController.page != null && _pageController.page!.round() > 0) {
+      _pageController.previousPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void _nextPage() async {
+    if (_pageController.page != null && _pageController.page!.round() == 1) {
+      bool isUsernameValid = await _checkUsernameValidity(_usernameController.text);
+      if (!isUsernameValid) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please enter a unique username')),
+        );
+        return;
+      }
+    }
+
+    if (_pageController.page != null && _pageController.page!.round() < 2) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
   }
 
   @override
@@ -65,23 +116,9 @@ class SetupJourneyState extends State<SetupJourney> {
           ),
         ],
       ),
-      bottomNavigationBar: NextBottomButton(
-        onPressed: () async {
-          if (_pageController.page?.round() == 1) {
-            bool isUsernameValid = await _checkUsernameValidity(_usernameController.text);
-            if (!isUsernameValid) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Please enter a unique username')),
-              );
-              return;
-            }
-          }
-
-          _pageController.nextPage(
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-          );
-        },
+      bottomNavigationBar: SetupJourneyFooter(
+        onBack: _goBack,
+        onNext: _nextPage,
       ),
     );
   }
