4dff35c89d5b88e7584aae090b356f138bd53abf
abuyukyi101198 <acb3@outlook.com>
2024-08-25 00:39:01 +0300

    removed padding from next_bottom_button.dart and added back_bottom_button.dart

---
+++ lib/widgets/buttons/back_bottom_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:sidelines/widgets/buttons/secondary_filled_button.dart';
+
+class BackBottomButton extends StatelessWidget {
+  final String? text;
+  final void Function()? onPressed;
+
+  final String _defaultText = 'Back';
+
+  const BackBottomButton({super.key, this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    final String effectiveText = text ?? _defaultText;
+    return SecondaryFilledButton(
+        onPressed: onPressed, child: Text(effectiveText));
+  }
+}
--- lib/widgets/buttons/next_bottom_button.dart
+++ lib/widgets/buttons/next_bottom_button.dart
@@ -11,18 +11,6 @@ class NextBottomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String effectiveText = text ?? _defaultText;
-    return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.end,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FilledButton(onPressed: onPressed, child: Text(effectiveText)),
-            const SizedBox(
-              height: 48,
-            ),
-          ],
-        ));
+    return FilledButton(onPressed: onPressed, child: Text(effectiveText));
   }
 }
