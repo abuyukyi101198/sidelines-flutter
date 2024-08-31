4c3162762718f29307a6ccdd9656c303cc341fdc
abuyukyi101198 <acb3@outlook.com>
2024-08-31 17:26:23 +0300

    added generator for position buttons

--- lib/screens/player_info_screen.dart
+++ lib/screens/player_info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sidelines/utils/color_palette.dart';
 import 'package:sidelines/widgets/buttons/position_button.dart';
 
 class PlayerInfoScreen extends StatefulWidget {
@@ -14,15 +15,41 @@ class PlayerInfoScreen extends StatefulWidget {
 }
 
 class PlayerInfoScreenState extends State<PlayerInfoScreen> {
+  final List<Map<String, dynamic>> positions = [
+    {'text': 'ST', 'top': 36.0},
+    {'text': 'LW', 'left': 102.0, 'top': 48.0},
+    {'text': 'RW', 'right': 102.0, 'top': 48.0},
+    {'text': 'CM', 'top': 80.0},
+    {'text': 'LB', 'left': 86.0, 'bottom': 86.0},
+    {'text': 'RB', 'right': 86.0, 'bottom': 86.0},
+    {'text': 'CB', 'bottom': 76.0},
+    {'text': 'GK', 'bottom': 38.0},
+  ];
+
   void _onPositionTapped(String position) {
     setState(() {
       if (widget.positionController.contains(position)) {
-        widget.positionController
-            .remove(position);
+        widget.positionController.remove(position);
       } else {
         widget.positionController.add(position);
       }
     });
+  }
+
+  String _displaySelectedPosition(Set<String> positionController) {
+    List<String> selected = [];
+    for (Map<String, dynamic> position in positions) {
+      if (positionController.contains(position['text'])) {
+        selected.add(position['text']);
+      }
+    }
+    if (selected.isNotEmpty) {
+      if (selected.length == positions.length) {
+        return 'ANY';
+      }
+      return selected.join('/');
+    }
+    return 'Select a position';
   }
 
   @override
@@ -32,99 +59,61 @@ class PlayerInfoScreenState extends State<PlayerInfoScreen> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Player Information',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Player Information',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Text(
+                'Finally, let’s set what position you play and with what number on your back.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/field.png',
+                    fit: BoxFit.contain,
                   ),
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                const Text(
-                  'Finally, let’s set what position you play and with what number on your back.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                  ),
-                ),
-                const SizedBox(
-                  height: 36,
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/field.png',
-                      fit: BoxFit.contain,
+                  for (var pos in positions)
+                    PositionButton(
+                      text: pos['text']!,
+                      isSelected:
+                          widget.positionController.contains(pos['text']!),
+                      onTap: () => _onPositionTapped(pos['text']!),
+                      top: pos['top'],
+                      left: pos['left'],
+                      right: pos['right'],
+                      bottom: pos['bottom'],
                     ),
-                    PositionButton(
-                        top: 36,
-                        text: 'ST',
-                        isSelected: widget.positionController.contains('ST'),
-                        onTap: () {
-                          _onPositionTapped('ST');
-                        }),
-                    PositionButton(
-                        left: 102,
-                        top: 48,
-                        text: 'LW',
-                        isSelected: widget.positionController.contains('LW'),
-                        onTap: () {
-                          _onPositionTapped('LW');
-                        }),
-                    PositionButton(
-                        right: 102,
-                        top: 48,
-                        text: 'RW',
-                        isSelected: widget.positionController.contains('RW'),
-                        onTap: () {
-                          _onPositionTapped('RW');
-                        }),
-                    PositionButton(
-                        top: 80,
-                        text: 'CM',
-                        isSelected: widget.positionController.contains('CM'),
-                        onTap: () {
-                          _onPositionTapped('CM');
-                        }),
-                    PositionButton(
-                        left: 86,
-                        bottom: 86,
-                        text: 'LB',
-                        isSelected: widget.positionController.contains('LB'),
-                        onTap: () {
-                          _onPositionTapped('LB');
-                        }),
-                    PositionButton(
-                        right: 86,
-                        bottom: 86,
-                        text: 'RB',
-                        isSelected: widget.positionController.contains('RB'),
-                        onTap: () {
-                          _onPositionTapped('RB');
-                        }),
-                    PositionButton(
-                        bottom: 76,
-                        text: 'CB',
-                        isSelected: widget.positionController.contains('CB'),
-                        onTap: () {
-                          _onPositionTapped('CB');
-                        }),
-                    PositionButton(
-                        bottom: 38,
-                        text: 'GK',
-                        isSelected: widget.positionController.contains('GK'),
-                        onTap: () {
-                          _onPositionTapped('GK');
-                        }),
-                  ],
+                ],
+              ),
+              Center(
+                child: Text(
+                  _displaySelectedPosition(widget.positionController),
+                  style: const TextStyle(
+                    color: ColorPalette.primaryColor,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
