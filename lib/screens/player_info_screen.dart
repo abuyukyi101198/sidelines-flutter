caad2634cf6119db310a8e004dd2ef3bf98f2f23
abuyukyi101198 <acb3@outlook.com>
2024-08-31 17:43:36 +0300

    encapsulated position_field.dart

--- lib/screens/player_info_screen.dart
+++ lib/screens/player_info_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sidelines/utils/color_palette.dart';
-import 'package:sidelines/widgets/buttons/position_button.dart';
+import 'package:sidelines/widgets/fields/position_field.dart';
 
 class PlayerInfoScreen extends StatefulWidget {
   final Set<String> positionController;
@@ -15,43 +14,6 @@ class PlayerInfoScreen extends StatefulWidget {
 }
 
 class PlayerInfoScreenState extends State<PlayerInfoScreen> {
-  final List<Map<String, dynamic>> positions = [
-    {'text': 'ST', 'top': 36.0},
-    {'text': 'LW', 'left': 102.0, 'top': 48.0},
-    {'text': 'RW', 'right': 102.0, 'top': 48.0},
-    {'text': 'CM', 'top': 80.0},
-    {'text': 'LB', 'left': 86.0, 'bottom': 86.0},
-    {'text': 'RB', 'right': 86.0, 'bottom': 86.0},
-    {'text': 'CB', 'bottom': 76.0},
-    {'text': 'GK', 'bottom': 38.0},
-  ];
-
-  void _onPositionTapped(String position) {
-    setState(() {
-      if (widget.positionController.contains(position)) {
-        widget.positionController.remove(position);
-      } else {
-        widget.positionController.add(position);
-      }
-    });
-  }
-
-  String _displaySelectedPosition(Set<String> positionController) {
-    List<String> selected = [];
-    for (Map<String, dynamic> position in positions) {
-      if (positionController.contains(position['text'])) {
-        selected.add(position['text']);
-      }
-    }
-    if (selected.isNotEmpty) {
-      if (selected.length == positions.length) {
-        return 'ANY';
-      }
-      return selected.join('/');
-    }
-    return 'Select a position';
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,36 +44,7 @@ class PlayerInfoScreenState extends State<PlayerInfoScreen> {
               const SizedBox(
                 height: 24,
               ),
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/field.png',
-                    fit: BoxFit.contain,
-                  ),
-                  for (var pos in positions)
-                    PositionButton(
-                      text: pos['text']!,
-                      isSelected:
-                          widget.positionController.contains(pos['text']!),
-                      onTap: () => _onPositionTapped(pos['text']!),
-                      top: pos['top'],
-                      left: pos['left'],
-                      right: pos['right'],
-                      bottom: pos['bottom'],
-                    ),
-                ],
-              ),
-              Center(
-                child: Text(
-                  _displaySelectedPosition(widget.positionController),
-                  style: const TextStyle(
-                    color: ColorPalette.primaryColor,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
+              PositionField(positionController: widget.positionController),
             ],
           ),
         ),
---
+++ lib/widgets/fields/position_field.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/color_palette.dart';
+import '../buttons/position_button.dart';
+
+class PositionField extends StatefulWidget {
+  final Set<String> positionController;
+
+  const PositionField({
+    super.key,
+    required this.positionController,
+  });
+
+  @override
+  PositionFieldState createState() => PositionFieldState();
+}
+
+class PositionFieldState extends State<PositionField> {
+  final List<Map<String, dynamic>> positions = [
+    {'text': 'ST', 'top': 36.0},
+    {'text': 'LW', 'left': 102.0, 'top': 48.0},
+    {'text': 'RW', 'right': 102.0, 'top': 48.0},
+    {'text': 'CM', 'top': 80.0},
+    {'text': 'LB', 'left': 86.0, 'bottom': 86.0},
+    {'text': 'RB', 'right': 86.0, 'bottom': 86.0},
+    {'text': 'CB', 'bottom': 76.0},
+    {'text': 'GK', 'bottom': 38.0},
+  ];
+
+  void _onPositionTapped(String position) {
+    setState(() {
+      if (widget.positionController.contains(position)) {
+        widget.positionController.remove(position);
+      } else {
+        widget.positionController.add(position);
+      }
+    });
+  }
+
+  String _displaySelectedPosition(Set<String> positionController) {
+    List<String> selected = [];
+    for (Map<String, dynamic> position in positions) {
+      if (positionController.contains(position['text'])) {
+        selected.add(position['text']);
+      }
+    }
+    if (selected.isNotEmpty) {
+      if (selected.length == positions.length) {
+        return 'ANY';
+      }
+      return selected.join('/');
+    }
+    return 'Select a position';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              'assets/images/field.png',
+              fit: BoxFit.contain,
+            ),
+            for (var pos in positions)
+              PositionButton(
+                text: pos['text']!,
+                isSelected:
+                widget.positionController.contains(pos['text']!),
+                onTap: () => _onPositionTapped(pos['text']!),
+                top: pos['top'],
+                left: pos['left'],
+                right: pos['right'],
+                bottom: pos['bottom'],
+              ),
+          ],
+        ),
+        Center(
+          child: Text(
+            _displaySelectedPosition(widget.positionController),
+            style: const TextStyle(
+              color: ColorPalette.primaryColor,
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
