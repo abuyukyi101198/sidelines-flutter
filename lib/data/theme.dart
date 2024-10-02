94f8cf3595916ff2d589ac7ac17b2d436733e8d6
abuyukyi101198 <acb3@outlook.com>
2024-10-02 20:57:00 +0300

    added theme.dart

---
+++ lib/data/theme.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GlobalTheme {
+  static Colors colors = const Colors();
+}
+
+class Colors {
+  const Colors();
+  Color get primaryColor => const Color.fromRGBO(224, 245, 115, 1);
+  Color get secondaryColor => const Color.fromRGBO(54, 54, 54, 1);
+
+  Color get textColor => const Color.fromRGBO(255, 255, 255, 1);
+
+  Color get backgroundColor => const Color.fromRGBO(25, 23, 23, 1);
+
+  Color get successColor => const Color.fromRGBO(45, 158, 43, 1);
+  Color get warningColor => const Color.fromRGBO(189, 182, 28, 1);
+  Color get errorColor => const Color.fromRGBO(158, 43, 43, 1);
+}
