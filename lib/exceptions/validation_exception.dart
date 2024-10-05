785f8debe796074d1ea547b97b3c2180b51a2634
abuyukyi101198 <acb3@outlook.com>
2024-10-05 22:17:56 +0300

    added single error message support

--- lib/exceptions/validation_exception.dart
+++ lib/exceptions/validation_exception.dart
@@ -3,7 +3,9 @@ import 'package:sidelines/exceptions/runtime_exception.dart';
 class ValidationException extends RuntimeException {
   final List<String> _messages;
 
-  ValidationException(this._messages);
+  ValidationException(List<String> messages) : _messages = messages;
+
+  ValidationException.single(String message) : _messages = [message];
 
   @override
   List<String> get messages => _messages;
