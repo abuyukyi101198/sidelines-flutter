1035abbcb4e307aff57718a71903f8328cc98d71
abuyukyi101198 <acb3@outlook.com>
2024-10-12 22:10:52 +0300

    added search_field.dart

---
+++ lib/widgets/fields/search_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:sidelines/data/theme.dart';
+
+class SearchField extends StatelessWidget {
+  final void Function(String)? onChanged;
+
+  const SearchField({
+    super.key,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle fieldStyle = TextStyle(
+        color: GlobalTheme.colors.textColor, fontSize: 12.0, height: 1.0);
+    InputDecoration decoration = InputDecoration(
+        filled: true,
+        fillColor: GlobalTheme.colors.secondaryColor,
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0),
+            borderSide: BorderSide.none),
+        isDense: true);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextField(
+          style: fieldStyle,
+          cursorColor: GlobalTheme.colors.textColor,
+          decoration: decoration,
+          onChanged: onChanged,
+        ),
+      ],
+    );
+  }
+}
