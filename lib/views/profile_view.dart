65f0e0c4aa63eb7903c13a84b3a131bce5d65d5b
abuyukyi101198 <acb3@outlook.com>
2024-10-13 17:30:20 +0300

    reverted refreshable_view.dart

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sidelines/providers/friends_provider.dart';
-import 'package:sidelines/views/refreshable_view.dart';
 import 'package:sidelines/widgets/displays/profile_header.dart';
 import 'package:sidelines/widgets/displays/profile_info_display.dart';
 import 'package:sidelines/widgets/displays/profile_performance_chart.dart';
 import 'package:sidelines/widgets/displays/profile_statistics_display.dart';
 import '../data/storage.dart';
-import '../models/profile_model.dart';
+import '../data/theme.dart';
 import '../providers/profile_provider.dart';
 import '../viewmodels/profile_view_model.dart';
 import '../widgets/navigation/screen_navigation_bar.dart';
@@ -57,8 +56,6 @@ class ProfileViewState extends State<ProfileView> {
 
   @override
   Widget build(BuildContext context) {
-    ProfileModel profile = profileProvider.profile as ProfileModel;
-
     return Scaffold(
       appBar: AppBar(
         scrolledUnderElevation: 0,
@@ -72,20 +69,47 @@ class ProfileViewState extends State<ProfileView> {
           ),
         ],
       ),
-      body: RefreshableView(
+      body: FutureBuilder(
         future: _profileFuture,
-        onRefresh: _refreshProfile,
-        child: ListView(
-          children: [
-            ProfileHeader(profileModel: profile),
-            ProfileInfoDisplay(profileModel: profile),
-            ProfileStatisticsDisplay(profileModel: profile),
-            const SizedBox(height: 24.0),
-            const ProfilePerformanceChart(
-              ratings: [6.4, 7.6, 7.2, 8.3, 9.1, 7.1],
-            ),
-          ],
-        ),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child: CircularProgressIndicator(
+              backgroundColor: GlobalTheme.colors.secondaryColor,
+              color: GlobalTheme.colors.primaryColor,
+            ));
+          } else if (snapshot.hasError) {
+            return Center(
+                child: IconButton(
+              onPressed: _refreshProfile,
+              icon: Icon(
+                Icons.refresh_rounded,
+                color: GlobalTheme.colors.primaryColor,
+                size: 36.0,
+              ),
+            ));
+          }
+
+          return SafeArea(
+              minimum: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: RefreshIndicator(
+                color: GlobalTheme.colors.primaryColor,
+                backgroundColor: GlobalTheme.colors.backgroundColor,
+                onRefresh: _refreshProfile,
+                child: ListView(
+                  children: [
+                    ProfileHeader(profileModel: profileProvider.profile!),
+                    ProfileInfoDisplay(profileModel: profileProvider.profile!),
+                    ProfileStatisticsDisplay(
+                        profileModel: profileProvider.profile!),
+                    const SizedBox(height: 24.0),
+                    const ProfilePerformanceChart(
+                      ratings: [6.4, 7.6, 7.2, 8.3, 9.1, 7.1],
+                    ),
+                  ],
+                ),
+              ));
+        },
       ),
       bottomNavigationBar: const ScreenNavigationBar(currentIndex: 4),
     );
